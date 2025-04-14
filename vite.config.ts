627b7995078e6b0ac8ce88f90c78aaa9511cfad1
Simcha <slapp62@gmail.com>
2025-04-14 17:49:53 +0300

    trying to fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import flowbiteReact from "flowbite-react/plugin/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   base: "/reactFinalProject/",
   plugins: [react(), tailwindcss(), flowbiteReact()],
