2bef188bf2ef359ad3a8b4b958eaa188c0a8c5d0
Simcha <slapp62@gmail.com>
2025-04-14 16:47:59 +0300

    github deploy

--- src/pages/error.pages.tsx
+++ src/pages/error.pages.tsx
@@ -1,5 +1,7 @@
 import { Button } from "flowbite-react";
 import { useNavigate } from "react-router-dom";
+import { BsFillEmojiTearFill } from "react-icons/bs";
+
 
 const Error404 = () => {
     const nav = useNavigate();
@@ -10,6 +12,7 @@ const Error404 = () => {
 
     return (
         <div className="flex flex-col justify-center gap-4 items-center m-auto p-2">
+            <BsFillEmojiTearFill size={100}/>
             <h1 className="text-3xl font-bold">404: Page Not Found</h1>
             <Button onClick={goHome}>Return to Homepage</Button>
         </div>
